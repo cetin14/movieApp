f28092d8b16d94cdf2ae6890419e70a3ce1bd13c
SebileGrgl <livaliva998@gmail.com>
2024-04-03 22:24:55 +0300

    database için bilgiler eklendi

--- js/app.js
+++ js/app.js
@@ -1,75 +1,106 @@
 films = [
-{ 
-    id:1,
+  {
+    id: 1,
     name: "Matrix",
     director: " Lana Wachowski, Lilly Wachowski",
     year: 1999,
     rating: 8.7,
     genre: "action",
-    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcgBJGoclqi7UrGpMr3meUb_OeZd8XovrTL2HLUSds7g&s",
-},
-{ 
-    id:2,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcgBJGoclqi7UrGpMr3meUb_OeZd8XovrTL2HLUSds7g&s",
+    trailer: "https://youtu.be/m8e-FF8MsqU?si=t9sKr120CrmjSlYc",
+    description:
+      " It is the first installment in the Matrix film series, starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano, and depicts a dystopian future in which humanity is unknowingly trapped inside the Matrix, a simulated reality that intelligent machines have created to distract humans while using their bodies as an energy source. When computer programmer Thomas Anderson, under the hacker alias 'Neo', uncovers the truth, he joins a rebellion against the machines along with other people who have been freed from the Matrix.",
+  },
+  {
+    id: 2,
     name: "The Lord of the Rings: The Fellowship of the Ring",
     director: "Peter Jackson",
     year: 2001,
     rating: 8.9,
     genre: "action",
-    image:"https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
-},
-{ 
-    id:3,
+    image:
+      "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
+    trailer: "https://youtu.be/V75dMMIW2B4?si=Jmwv1L5t2BQoANNU",
+    description:
+      "The Fellowship of the Ring consists of nine walkers who set out on the quest to destroy the One Ring, in opposition to the nine Black Riders: Frodo Baggins, Sam Gamgee, Merry Brandybuck and Pippin Took; Gandalf; the Men Aragorn and Boromir, son of the Steward of Gondor; the Elf Legolas; and the Dwarf Gimli.",
+  },
+  {
+    id: 3,
     name: "The Shawshank Redemption",
     director: "Frank Darabont",
     year: 1994,
     rating: 9.3,
     genre: "drama",
-    image:"https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg",
-},
-{ 
-    id:4,
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg",
+    trailer: "https://youtu.be/PLl99DlL6b4?si=r5izCzlldAoeRKa0",
+    description:
+      "Synopsis. In 1947, Andy Dufresne (Tim Robbins), a banker in Maine, is convicted of murdering his wife and her lover, a golf pro. Since the state of Maine has no death penalty, he is given two consecutive life sentences and sent to the notoriously harsh Shawshank Prison.",
+  },
+  {
+    id: 4,
     name: "The Godfather",
     director: "Francis Ford Coppola",
     year: 1972,
     rating: 9.2,
     genre: "Crime",
-    image:"https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",
-},
-{ 
-    id:5,
+    image:
+      "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",
+    trailer: "https://youtu.be/UaVTIH8mujA?si=cFT9s1abGlWHRJlW",
+    description:
+      "The Godfather tells the story of the Corleone Family, in which the primary protagonists are the leaders of a New York-based Mafia organization—first Don Vito Corleone and then his son, Michael Corleone.",
+  },
+  {
+    id: 5,
     name: "The Dark Knight",
     director: "Christopher Nolan",
     year: 2008,
     rating: 9.0,
     genre: "action",
-    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROE5xwAH1C5P47qMAyG2rffn9KF4JjWks7sbBXPy1TJw&s",
-},
-{ 
-    id:6,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROE5xwAH1C5P47qMAyG2rffn9KF4JjWks7sbBXPy1TJw&s",
+    trailer: "https://youtu.be/EXeTwQWrcwY?si=HLtxkGa4aV6LthKr",
+    description:
+      "The plot follows the vigilante Batman, police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City. Their efforts are derailed by the Joker, an anarchistic mastermind who seeks to test how far Batman will go to save the city from chaos.",
+  },
+  {
+    id: 6,
     name: "Forrest Gump",
     director: "Robert Zemenckis",
     year: 1994,
     rating: 8.8,
     genre: "romance",
-    image:"https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
-},
-{ 
-    id:7,
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
+    trailer: "https://youtu.be/bLvqoHBptjg?si=xCUF4sFgHHaZodcS",
+    description:
+      "The history of the United States from the 1950s to the '70s unfolds from the perspective of an Alabama man with an IQ of 75, who yearns to be reunited with his childhood sweetheart.",
+  },
+  {
+    id: 7,
     name: "Fight Club",
     director: "David Fincher",
     year: 1999,
     rating: 8.8,
     genre: "drama",
-    image:"https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
-},
-{ 
-    id:8,
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    trailer: "https://youtu.be/SUXWAEX2jlg?si=oAbWQyA1G3K4I5l_",
+    description:
+      "It is based on the 1996 novel by Chuck Palahniuk. Norton plays the unnamed narrator, who is discontented with his white-collar job. He forms a 'fight club' with soap salesman Tyler Durden (Pitt), and becomes embroiled in a relationship with an impoverished but beguilingly attractive woman, Marla Singer (Bonham Carter).",
+  },
+  {
+    id: 8,
     name: "Goodfellas",
     director: "Martin Scorsese",
     year: 1990,
     rating: 8.7,
     genre: "biography",
-    image:"https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
-},
+    image:
+      "https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    trailer: "https://youtu.be/2ilzidi_J8Q?si=t9evFJ9PhyxqCML3",
+    description:
+      "The story of Henry Hill and his life in the mafia, covering his relationship with his wife Karen and his mob partners Jimmy Conway and Tommy DeVito. The lowly, blue-collar side of New York's Italian mafia is explored in this crime biopic of wiseguy Henry Hill.",
+  },
 ];
-
